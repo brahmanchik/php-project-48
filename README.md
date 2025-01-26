af2e8278bf9058ea53cda3b78e1ec0ac87e519ec
Dmitry Tarasov <brahmanchik@gmail.com>
2025-01-26 22:47:41 +0300

    Implemented searching for differences between two flat json files.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/brahmanchik/php-project-48/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/brahmanchik/php-project-48/actions)
+
+# Differ
+
+A package to compare JSON files and show differences.
+
+## Example of usage
+
+[![asciinema](https://asciinema.org/a/07kSTaKSslkRcP3CNOJDAb78J)](https://asciinema.org/a/07kSTaKSslkRcP3CNOJDAb78J)
--- src/Differ.php
+++ src/Differ.php
@@ -1,12 +1,14 @@
 <?php
 namespace Differ\Differ;
+use function Functional\sort;
 function genDiff($pathToFile1, $pathToFile2)
 {
     $decodeFiles = [
         'contentFile1' => readAndDecodeJson($pathToFile1),
         'contentFile2' => readAndDecodeJson($pathToFile2),
     ];
-    return $decodeFiles;
+
+    print_r(renderDiff($decodeFiles) . PHP_EOL); //вывод сравнения строк в двух файлах
 }
 function readAndDecodeJson($pathToFile)
 {
@@ -16,4 +18,41 @@ function readAndDecodeJson($pathToFile)
     }
     $content = file_get_contents($absolutePath);
     return json_decode($content, true); // возвращаю содержимое переданного файла в массива
+}
+function renderDiff($filesContent)
+{
+    $diff = [];
+    $contentFile1 = $filesContent['contentFile1'];
+    $contentFile2 = $filesContent['contentFile2'];
+    $keys = array_keys(array_merge($contentFile1, $contentFile2));
+    $keysSort = sort($keys, fn ($left, $right) => strcmp($left, $right), true);
+    foreach ($keysSort as $key) {
+        $value1 = $contentFile1[$key] ?? null;
+        $value2 = $contentFile2[$key] ?? null;
+        // Преобразование значений true и false в строковый тип
+        $value1Formatted = formatValue($value1);
+        $value2Formatted = formatValue($value2);
+       if (array_key_exists($key, $contentFile1) && !array_key_exists($key, $contentFile2)) {
+           $diff[] = "- $key: $value1Formatted"; //эта строчка только в первом файле
+        } elseif (array_key_exists($key, $contentFile2) && !array_key_exists($key, $contentFile1)) {
+            $diff[] = "+ $key: $value2Formatted"; //эта строчка только в втором файле
+        } elseif ($contentFile1[$key] === $contentFile2[$key]) {
+           $diff[] = "  $key: $value1Formatted"; //эта строчка есть в обоих файлах
+       } else {
+           $diff[] = "- $key: $value1Formatted"; //эта строчка только в первом файле
+           $diff[] = "+ $key: $value2Formatted"; //эта строчка только в втором файле
+       }
+    }
+    return implode("\n", $diff);
+}
+
+function formatValue($value)
+{
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false'; // Преобразование булевых значений в строки
+    }
+    if (is_null($value)) {
+        return 'null'; // Преобразование null
+    }
+    return $value; // Остальные значения возвращаются как есть
 }
