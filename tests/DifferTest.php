7869625d633868c898e8c5ec2fae55901bdb231f
Dmitry Tarasov <brahmanchik@gmail.com>
2025-02-02 21:55:39 +0300

    edit file show-directory.yml

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -12,12 +12,10 @@ class DifferTest extends TestCase
     {
         $pathFile1 = __DIR__ . '/fixtures/file1.json';
         $pathFile2 = __DIR__ . '/fixtures/file2.json';
-        $pathFileExpected1 = __DIR__ . '/fixtures/Expected1';
+        $pathFileExpected1 = __DIR__ . '/fixtures/expected1';
         $resultGenDiff = genDiff($pathFile1, $pathFile2);
         $expected1 = file_get_contents($pathFileExpected1);
         //dd($expected1, $resultGenDiff);
-        var_dump(realpath($pathFileExpected1), file_exists($pathFileExpected1), file_get_contents($pathFileExpected1));
-        exit;
         $this->assertEquals($expected1, $resultGenDiff);
     }
 }
